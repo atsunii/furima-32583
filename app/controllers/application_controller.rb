895780ddf389ef3bd9af9e8736aa8c639b4cbf94
atsunii <tky3a1938@gmail.com>
2020-12-22 17:05:43 +0900

    修正１

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :kanji_last_name, :kanji_first_name, :katakana_last_name, :katakana_first_name, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :kanji_last_name, :kanji_first_name, :katakana_last_name,
+                                             :katakana_first_name, :birthday])
   end
 end
--- spec/models/streetaddress_spec.rb
+++ spec/models/streetaddress_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Streetaddress, type: :model do
-
 end
