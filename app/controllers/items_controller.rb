5c8445f82d63cc7397f245dbebb3e101b18b7bdc
atsunii <tky3a1938@gmail.com>
2021-01-04 11:41:26 +0900

    訂正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,13 +6,13 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item = Item.new
+    @items = Item.new
   end
 
   def create
     @item = Item.new(item_params)
-    if @items.save
-      redirect_to item_path(@item.id)
+    if @item.save
+      redirect_to items_path(@item.id)
     else
       render :new
     end
